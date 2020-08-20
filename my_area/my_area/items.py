61157af77ec01c916109ef62784a437e3790fda3
version07 <version07@naver.com>
2020-08-20 12:22:24 +0900

    selenium

--- my_area/my_area/items.py
+++ my_area/my_area/items.py
@@ -3,7 +3,6 @@ import scrapy
 
 class My_areaItem(scrapy.Item):
     title = scrapy.Field()
-    good = scrapy.Field()
     comment = scrapy.Field()
     day = scrapy.Field()
-    link = scrapy.Field()    
+    link = scrapy.Field()
